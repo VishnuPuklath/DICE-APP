690000a08abf93e7b1904a9b1bd1a8228099e072
vishnu <vishnupgithub@gmail.com>
2022-04-29 12:23:08 +0530

    Dice rolling added

--- lib/dicepage.dart
+++ lib/dicepage.dart
@@ -1,25 +1,66 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class DicePage extends StatefulWidget {
-  const DicePage({Key? key}) : super(key: key);
-
   @override
   State<DicePage> createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title: Text(
-          'Dice',
-          style: TextStyle(fontWeight: FontWeight.bold),
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Text(
+            'Dice',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Row(),
-    );
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GestureDetector(
+                      onTap: () {
+                        leftDiceChange();
+                      },
+                      child: Image.asset('assets/dice$leftDiceNumber.png')),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GestureDetector(
+                      onTap: () {
+                        rightDiceChange();
+                      },
+                      child: Image.asset('assets/dice$rightDiceNumber.png')),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+
+  void rightDiceChange() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+
+    
+  }
+
+  void leftDiceChange() {
+    setState(() {
+       leftDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
